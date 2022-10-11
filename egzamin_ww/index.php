e0223e7f147bbd08286a137c3aea91aae140d754
KartGameDev <106374290+KartGameDev@users.noreply.github.com>
2022-10-11 15:21:04 +0200

    poprawki

--- egzamin_ww/index.php
+++ egzamin_ww/index.php
@@ -1,13 +1,33 @@
 <?php
-    $data = $numer = $zgoda = ""
+    $data = $numer = $zgoda = "";
 
-    function spr ($test){
-        $test = trim($ )
+    function dod($server, $username, $password, $database, $array) {
+        $db = new mysqli($server, $username, $password, $database);
+
+        $sql = "INSERT INTO rezerwacje (data_rez, telefon) VALUES ('{$array[0]}', {$array[1]}, '{$array[2]}')";
+        $db->query($sql);
+        $db->close();
+    }
+
+    function spr($test){
+        $test = trim($test);
+        $test = stripslashes($test);
+        $test = htmlspecialchars($test);
+        return $test;
     }
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $data = spr($_POST["data"]);
+        $numer = spr($_POST["numer"]);
+        $zgoda = spr($_POST["zgoda"]);
 
+        if (dod("localhost", "root", "", "baza", array($data, $numer)) == NULL) {
+            echo "dodano rezerwację";
+        } else {
+            echo "nie dodano rezerwacji";
+        }
     }
+
 
 
 ?>
