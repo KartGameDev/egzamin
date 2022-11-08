ab145ce50f31cdcb7c369de60919e959e9289974
KartGameDev <106374290+KartGameDev@users.noreply.github.com>
2022-11-08 11:51:54 +0100

    egzamin2

---
+++ spr2/wedkuj.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>Wędkowanie</title>
+    <link rel="stylesheet" type="text/css" href="styl_1.css" />
+</head>
+<body>
+    <div id="header">
+        <h1>Portal dla wędkarzy</h1>
+    </div>
+    <div id="main">
+        <div id="lewe">
+            <div id="lewy1">
+                <h3>Ryby zamieszkujące rzeki</h3>
+                <ol>
+                    <?php
+                    ?>
+                </ol>
+            </div>
+            <div id="lewy2">
+                <h3>Ryby drapieżne naszych wód</h3>
+                <table>
+                    <tr>
+                        <th>L.p.</th>
+                        <th>Gatunek</th>
+                        <th>Występowanie</th>
+                    </tr>
+                </table>
+            </div>
+        </div>
+
+        <div id="prawy">
+        <img src="ryba1.jpg">
+        <li><a href="kwerendy.txt">Pobierz kwerendy</a></li>
+        </div>
+    </div>
+    <div id="footer">
+        <p>Stronę wykonał: 00000000000</p>
+    </div>
+</body>
+</html>
